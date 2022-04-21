fe0c9575ed67761b65a5b1df095e4d86bc25d21b
Jan-Erik Rediger <janerik@fnordig.de>
2022-04-21 15:24:26 +0200

    v50.0.0

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 // swift-tools-version:5.4
 import PackageDescription
 
-let checksum = "4c6a7f45db869d3d2da52a7f999045ba9c7935e59a4736bef7f8324c83a5191a"
-let version = "44.1.1"
-let url = "https://github.com/mozilla/glean/releases/download/v\(version)/Glean.xcframework.zip"
+let checksum = "4f02cb220a219e9beb17649c4d91914847f3b812455c6c3e05ad89f75405a468"
+let version = "50.0.0"
+let url = "https://github.com/badboy/glean2-swift/releases/download/\(version)/Glean.xcframework.zip"
 
 let package = Package(
     name: "Glean",
